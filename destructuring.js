d33acf41dbe70ef83881e6cf41651daea4862e33
Hunter Carrico <40701152+hmcarrico@users.noreply.github.com>
2018-08-08 21:33:24 -0700

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -23,7 +23,7 @@ var carDetails = {
 
 //Code Here
 
-
+const {color, make, model, year} = carDetails;
 
 ////////// PROBLEM 2 //////////
 
@@ -35,7 +35,7 @@ var carDetails = {
 
 function greeting( obj ) {
   //Code Here
-  
+  const {firstName, lastName, title} = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -55,7 +55,11 @@ function greeting( obj ) {
 
 //Code Here
 
+function totalPopulation( obj ) {
+  var { utah, california, texas, arizona } = obj;
+  return utah + california + texas + arizona;
 
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -69,7 +73,12 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function ingredients( obj ) {
+  let arr = [];
+  const {carb, fat, protein} = obj;
+  arr.push(carb, fat, protein)
+  return arr;
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -87,7 +96,15 @@ function greeting( obj ) {
 
 //Code Here
 
-
+function largeNumbers( {first, second, third} ) {
+  if(first < second && first < third){
+    return first;
+  } else if(second < third && second < first) {
+    return second;
+  } else{
+    return third;
+  }
+}
 
 ////////// PROBLEM 6 //////////
 
@@ -98,5 +115,12 @@ function greeting( obj ) {
 */
 
 //Code Here
-
-
+function numberGroups( {a,b,c} ){
+  if(a.length > b.length && a.length > c.length){
+    return a;
+  } else if(b.length > c.length && b.length > a.length) {
+    return b;
+  } else{
+    return c;
+  }
+}
