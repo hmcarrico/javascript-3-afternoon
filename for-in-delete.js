9c20b6032f94e68dfd1df2ef392bfabfe7916ed1
Hunter Carrico <40701152+hmcarrico@users.noreply.github.com>
2018-08-08 21:33:34 -0700

    Update for-in-delete.js

--- for-in-delete.js
+++ for-in-delete.js
@@ -40,7 +40,11 @@
 */
 
 function showValues( obj ) {
-  //Code Here
+  let arr = []; 
+  for(var x in obj) {
+    arr.push(obj[x])
+  }
+  return arr.join("");
 }
 
 
@@ -55,7 +59,14 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function greaterThan10( obj ){
+  for(let x in obj){
+    if(obj[x]>10){
+      obj[x]=0;
+    }
+  }
+  return obj;
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -67,7 +78,12 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function double( obj ){
+  for(let x in obj){
+    obj[x] = obj[x]*2
+  }
+  return obj;
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -81,7 +97,15 @@ function showValues( obj ) {
 
 //Code Here
 
-
+function secrets(obj){
+  let emptyStr = "";
+  for(let x in obj){
+    if(x.startsWith("sh")){
+      emptyStr += obj[x];
+    }
+  }
+  return emptyStr;
+}
 
 /* 
   Sometimes it's needed to delete object properties. 
@@ -112,6 +136,15 @@ function showValues( obj ) {
 
 //Code Here
 
+function removePassword(obj){
+  for(let x in obj){
+    if(x === "password"){
+      delete obj[key];
+    }
+  }
+  return obj;
+}
+
 
 
 ////////// PROBLEM 6 //////////
@@ -131,7 +164,16 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
+function removeBig(obj){
+  for(let x in obj){
+    if(obj[x]>100){
+      delete obj[x];
+    }
+  }
+  return obj;
+}
 
+removeBig(deleteTheBigNumbers);
 
 ////////// PROBLEM 7 //////////
 
@@ -144,7 +186,14 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
-
+function startsWithK(obj){
+  for(let x in obj){
+    if(x.startsWith("k")){
+      delete obj[x];
+    }
+  }
+  return obj;
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -159,4 +208,12 @@ var deleteTheBigNumbers = {
 
 //Code Here
 
+function hiddenTreasure(obj){
+  for(let x in obj){
+    if(!obj[x].includes("treasure")){
+      delete obj[x];
+    }
+  }
+  return obj;
+}
 
